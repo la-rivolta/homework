95ac0014ffa5dd25ff360929941fb43687c765b8
la-rivolta <NadezhdaBilay@gmail.com>
2025-09-06 19:51:15 +0300

    Header Wake&Bake

---
+++ lesson127/js/main.js
@@ -0,0 +1,23 @@
+(function() {
+
+
+    const burgerInit = (event) => {
+
+        const burgerIcon = event.target.closest('.burger-icon');
+        const burgerNavLink = event.target.closest('.nav__link');
+
+        if(!burgerIcon && !burgerNavLink) return;
+
+        if(document.documentElement.clientWidth > 900) return;
+
+        if(!document.body.classList.contains('body--opened-menu')){
+            document.body.classList.add('body--opened-menu');
+        } else {
+            document.body.classList.remove('body--opened-menu');
+        }
+
+    }
+
+    document.addEventListener('click', burgerInit);
+
+})()
